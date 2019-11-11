3162454f5f98a2f086c974881a36d6cc9cf8c1a8
hamedasemi <h-@live.se>
2019-11-11 15:35:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lit-element-router
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/lit-element-router)
---
+++ my-app.js
@@ -0,0 +1,70 @@
+/**
+ * LitElement Router Pages
+ * 
+ * https://github.com/hamedasemi/lit-element-router
+ * https://www.npmjs.com/package/lit-element-router
+ */
+
+import { } from '@webcomponents/webcomponentsjs/webcomponents-loader.js'
+
+import { LitElement, html } from 'lit-element'
+import { routerMixin } from 'lit-element-router'
+
+import './app-link';
+import './app-main';
+
+class MyApp extends routerMixin(LitElement) {
+
+  static get properties() {
+    return {
+      route: { type: String },
+      params: { type: Object }
+    }
+  }
+
+  static get routes() {
+    return [{
+      name: 'home',
+      pattern: '',
+      data: { title: 'Home' }
+    }, {
+      name: 'info',
+      pattern: 'info'
+    }, {
+      name: 'user',
+      pattern: 'user/:id'
+    }, {
+      name: 'not-found',
+      pattern: '*'
+    }];
+  }
+
+  constructor() {
+    super();
+    this.route = '';
+    this.params = {};
+  }
+
+  onRoute(route, params, query, data) {
+    this.route = route;
+    this.params = params;
+    console.log(route, params, query, data)
+  }
+
+  render() {
+    return html`
+      <app-link href="/">Home</app-link>
+      <app-link href="/info">Info</app-link>
+      <app-link href="/user/14">user/14</app-link>
+
+      <app-main current-route=${this.route}>
+          <h1 route='home'>Home</h1>
+          <h1 route='info'>Info</h1>
+          <h1 route='user'>User ${this.params.id} </h1>
+          <h1 route='not-found'>Not Found </h1>
+      </app-main>
+    `
+  }
+}
+
+customElements.define('my-app', MyApp);
