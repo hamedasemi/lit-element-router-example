54305b7ed9e318e498bda168c81a39b7925ca4dd
hamedasemi <h-@live.se>
2019-11-19 11:21:42 +0100

    Add query preview

--- my-app.js
+++ my-app.js
@@ -18,7 +18,8 @@ class App extends routerMixin(LitElement) {
   static get properties() {
     return {
       route: { type: String },
-      params: { type: Object }
+      params: { type: Object },
+      query: { type: Object }
     }
   }
 
@@ -43,11 +44,13 @@ class App extends routerMixin(LitElement) {
     super();
     this.route = '';
     this.params = {};
+    this.query = {};
   }
 
   router(route, params, query, data) {
     this.route = route;
     this.params = params;
+    this.query = query;
     console.log(route, params, query, data)
   }
 
@@ -55,11 +58,12 @@ class App extends routerMixin(LitElement) {
     return html`
       <app-link href="/">Home</app-link>
       <app-link href="/info">Info</app-link>
+      <app-link href="/info?data=12345">Info?data=12345</app-link>
       <app-link href="/user/14">user/14</app-link>
 
       <app-main active-route=${this.route}>
           <h1 route='home'>Home</h1>
-          <h1 route='info'>Info</h1>
+          <h1 route='info'>Info ${this.query.data}</h1>
           <h1 route='user'>User ${this.params.id} </h1>
           <h1 route='not-found'>Not Found </h1>
       </app-main>
